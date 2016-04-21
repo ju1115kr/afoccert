f335d1b7729588db2393661413443351c9aeb7dc
Kyu-Hyun Shin <shark@localhost.localdomain>
2016-04-21 20:31:50 +0900

    bugfix: add expose location header decorator to tags post endpoint

--- app/api_1_0/tag.py
+++ app/api_1_0/tag.py
@@ -31,6 +31,7 @@ def get_tag(tag_name):
 # TODO: 관리자급 계정만 가능하게
 @api.route('/tags', methods=['POST'])
 @auth.login_required
+@cross_origin(expose_headers='Location')
 def post_tag():
     if request.json is None:
         return bad_request('JSON Request is invaild')
