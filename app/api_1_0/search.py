514fceb02f296fbde0a95c24e563f3729cb84965
이주영 <ju1115kr@localhost.localdomain>
2016-06-26 09:29:59 +0900

    add functional pagination in search result

--- app/api_1_0/search.py
+++ app/api_1_0/search.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import request, jsonify, make_response, url_for, g
+from flask import request, jsonify, g
 from . import api
 from authentication import auth
 from .. import db
@@ -21,12 +21,19 @@ def search_news(context):
 		return bad_request('Request is invaild')
 
 	context = removeEscapeChar(context)
-	search_result = News.query.filter(News.context.like('%'+context+'%')).order_by(News.id.desc()).all()
 
-	if search_result is None:
-		return not_found('News does not exist')
-	return jsonify({'news':[news.to_json() for news in search_result]})
+	context = removeEscapeChar(context)
 
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 1000, type=int)
+        pagination = News.query.filter(News.context.like('%'+context+'%'))\
+			.order_by(News.id.desc())\
+			.paginate(page, per_page, error_out=False)
+	pag_result = pagination.items
+
+	if pagination is None:
+		return not_found('Result does not exist')
+	return jsonify({'news':[news.to_json() for news in pag_result]})
 
 @api.route('/search/comments/<context>', methods=['GET'])
 @auth.login_required
@@ -35,15 +42,21 @@ def search_comment(context):
 		return bad_request('Request is invaild')
 
 	context = removeEscapeChar(context)
-	search_result = Comment.query.filter(Comment.context.like('%'+context+'%')).order_by(Comment.news_id.desc()).all()
-	if search_result is None:
-		return not_found('Comment does not exist')
-	return jsonify({'comments':[comment.to_json() for comment in search_result]})
 
+	page = request.args.get('page', 1, type=int)
+	per_page = request.args.get('per_page', 1000, type=int)
+	pagination = Comment.query.filter(Comment.context.like('%'+context+'%'))\
+		.order_by(Comment.news_id.desc())\
+		.paginate(page, per_page, error_out=False)
+	pag_result = pagination.items
+
+	if pagination is None:
+		return not_found('Comment does not exist')
+	return jsonify({'comments':[comment.to_json() for comment in pag_result]})
 
 @api.route('/search/', methods=['GET'])
 @auth.login_required
-def search_date():
+def search_allmight():
 	startpoint = request.args.get('startpoint')
 	endpoint = request.args.get('endpoint')
 	context = request.args.get('context')
@@ -54,14 +67,23 @@ def search_date():
 		startpoint = "2016-02-04" #At first news
 	if endpoint is None:
 		endpoint = datetime.utcnow()
+
+	page = request.args.get('page', 1, type=int)
+	per_page = request.args.get('per_page', 1000, type=int)
+
 	if context is None:
-		search_result = News.query.filter(endpoint > News.created_at)\
-				.filter(News.created_at > startpoint).order_by(News.id.desc()).all()
+		pagination = News.query.filter(endpoint > News.created_at)\
+			.filter(News.created_at > startpoint).order_by(News.id.desc())\
+			.paginate(page, per_page, error_out=False)
+		 pag_result = pagination.items
 
 	search_result = News.query.filter(endpoint > News.created_at)\
 			.filter(News.created_at > startpoint)\
-			.filter(News.context.like('%'+context+'%')).order_by(News.id.desc()).all()
- 
-	if search_result is None:
+			.filter(News.context.like('%'+context+'%')).order_by(News.id.desc())\
+			.paginate(page, per_page, error_out=False)
+
+	pag_result = pagination.items
+
+	if pag_result is None:
 		return not_found('News does not exist')
-	return jsonify({'news':[news.to_json() for news in search_result]})
+	return jsonify({'news':[news.to_json() for news in pag_result]})
